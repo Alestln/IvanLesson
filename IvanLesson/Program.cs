00938503d5645e4a40e4127e65afe36888ecff89
Alestln <itworkplace46@gmail.com>
2025-03-15 17:30:13 +0200

    empty Main

--- IvanLesson/Program.cs
+++ IvanLesson/Program.cs
@@ -4,50 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        // Output data
-
-        // int number = 42;
-        // Console.WriteLine("My number: " + number + ".");
-        // Console.WriteLine("My number: {0}.", number);
-        // Console.WriteLine($"My number: {number}.");
-
-        // Console.WriteLine("Hello");
-
-        // Input data
-
-        // string name = Console.ReadLine();
-
-        //int age = Convert.ToInt32(Console.ReadLine());
-
-        /*int age;
-        while (true)
-        {
-            try
-            {
-                age = Convert.ToInt32(Console.ReadLine());
-
-                break;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }*/
-
-        // int age = int.Parse(Console.ReadLine());
-
-        int age;
-
-        while (true)
-        {
-            if (int.TryParse(Console.ReadLine(), out age))
-            {
-                break;
-            }
-
-            Console.WriteLine("ERROR");
-        }
-
-        Console.WriteLine($"My age: {age}.");
+        
     }
 }
